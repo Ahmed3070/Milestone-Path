5ec58bb18ca54265e3a7725ce5d58809148cd12b
Ahmed <shaikahmed3070@gmail.com>
2024-10-20 23:14:30 +0530

    modified

--- script.js
+++ script.js
@@ -2,3 +2,25 @@ const timeline =document.getElementById("timeline");
 const prev = document.getElementById("prev");
 const next = document.getElementById("next");
 const circles = document.querySelectorAll(".circle");
+
+let step =1;
+
+function mainCode() {
+
+}
+
+next.addEventListener("click",()=> {
+    step++;
+    if(step > circles.length){
+        step = circles.length;
+    }
+    mainCode();
+});
+
+prev.addEventListener("click",()=> {
+    step--;
+    if(step < 1) {
+        step = 1;
+    }
+    mainCode();
+});
