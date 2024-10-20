8a484dd95a0869cd94fe656fee8cb4faf8b20d63
Ahmed <shaikahmed3070@gmail.com>
2024-10-20 23:31:43 +0530

    modified code

--- script.js
+++ script.js
@@ -6,7 +6,27 @@ const circles = document.querySelectorAll(".circle");
 let step =1;
 
 function mainCode() {
+    circles.forEach((circles,index)=> {
+        if(index < step) {
+            circles.classList.add("active");
+        } else {
+            circles.classList.remove("active");
+        }
+    });
 
+    const actives = document.querySelectorAll(".active");
+    timeline.style.width = `${
+        ((actives.length -1)/(circles.length -1))*100
+    }%`;
+
+    if(step === 1){
+        prev.disabled= true;
+    } else if(step === circles.length){
+        next.disabled = true;
+    } else {
+        prev.disabled = false;
+        next.disabled = false;
+    }
 }
 
 next.addEventListener("click",()=> {
