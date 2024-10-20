fcb0180a03be2f7e689d6a137e1fda03a38d4142
Ahmed <shaikahmed3070@gmail.com>
2024-10-20 23:10:17 +0530

    modified

---
+++ script.js
@@ -0,0 +1,4 @@
+const timeline =document.getElementById("timeline");
+const prev = document.getElementById("prev");
+const next = document.getElementById("next");
+const circles = document.querySelectorAll(".circle");
